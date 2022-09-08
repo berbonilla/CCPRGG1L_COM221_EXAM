0026e2aca4693e22ad9c486a3115e8319af8496e
Iber Joseph Bonilla <111488477+berbonilla@users.noreply.github.com>
2022-09-08 14:23:25 +0800

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
     
- 
+    Students Me = new Students("Bonilla","Iber Joseph",'P',"03/27/2003");
+    Me.Values();
+
+    Me.studentEmailAddress = "bonilaip@student.national-u.edu.ph";
+    Me.studentNumber = 2022102503;
+    Me.isAwesome = true;
+
+    Me.sayMyemailAddress();
+    Me.sayStudentNumber();
+    Me.sayMyAwesomeness();
     }
 }
