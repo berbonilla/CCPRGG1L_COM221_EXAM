2cf16faab63da5c856c76d040e4f8b0ebdfa2d5e
Iber Joseph Bonilla <111488477+berbonilla@users.noreply.github.com>
2022-11-10 15:44:16 +0800

    Update Final_Exam.java

--- Exam 3/Final_Exam.java
+++ Exam 3/Final_Exam.java
@@ -1,10 +1,5 @@
-import java.util.Scanner;
-
-public class App {
+public class Main {
     public static void main(String[] args) throws Exception {
-
-        Scanner sc = new Scanner(System.in);
-
         // Print the words in their given pattern.
 
         char[][] array = {
